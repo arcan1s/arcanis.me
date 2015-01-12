1786101d0d7a0b2a69fc245dd23fb40f66894d44
arcan1s <darkarcanis@mail.ru>
2015-01-12 19:06:35 +0300

    update netctl-gui to 1.4.0

---
+++ devs/netctl-gui/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "acdefginrsw~",
+  1: "nw",
+  2: "nw",
+  3: "acefginrsw~",
+  4: "adens"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables"
+};
+
